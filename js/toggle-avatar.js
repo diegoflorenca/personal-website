c3dda7f934165e1d8eca552ec193a99335a8b631
Diego Felipe Florença <diegoflorenca@gmail.com>
2022-04-30 21:24:28 -0700

    Correcting menu style bug

--- js/toggle-avatar.js
+++ js/toggle-avatar.js
@@ -1,12 +1,12 @@
 const scrollOffset = 500;
-const primaryHeader = document.querySelector('.primary-header');
+const fixedHeader = document.querySelector('.fixed-header');
 const avatar = document.querySelector('#avatar');
 
 window.addEventListener('scroll', () => {
 	if (window.scrollY > 10) {
-		primaryHeader.classList.add('active');
+		fixedHeader.classList.add('active');
 	} else {
-		primaryHeader.classList.remove('active');
+		fixedHeader.classList.remove('active');
 	}
 
 	if (window.scrollY > scrollOffset) {
